973fd79d384641c090295b50a01de080254dea07
starkDbl07 <abhi.emailto@gmail.com>
2015-07-17 11:32:20 +0545

    added info to initGit

--- initGit.sh
+++ initGit.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 
+# Author    :	Abhishek S. Okheda <abhi.emailto@gmail.com>
+# Purpose   :	Init Git repo
+# Date	    :	Fri Jul 17 11:31:57 NPT 2015
+
 mkdir -p .git .git/{refs,objects} 
 echo "ref: refs/heads/master" > .git/HEAD
